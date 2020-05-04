7ea4ada114d3b511b5f718945f4704b1dd553c7b
AbeTmohiro <tomohiro.abe@di-v.co.jp>
2020-05-04 20:31:27 +0900

    Send a message

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,67 @@
+	
+$(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html =
+        `<div class="message">
+          <div class="message__upper-info">
+            <div class="message__upper-info__talker">
+              ${message.user_name}
+            </div>
+            <div class="message__upper-info__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message">
+            <p class="message__text">
+              ${message.content}
+            </p>
+            <img class="Message__image" src="${message.image}">
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="message">
+        <div class="message__upper-info">
+          <div class="message__upper-info__talker">
+            ${message.user_name}
+          </div>
+          <div class="message__upper-info__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message">
+          <p class="message__textt">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
+  $('.new-message').on('submit', function(e){
+    e.preventDefault();
+    let formData = new FormData(this);
+    console.log(formData);
+    let url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.messages').append(html);      
+      $('form')[0].reset();
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      $('.submit-btn').prop('disabled',false)
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
+  });
+});
