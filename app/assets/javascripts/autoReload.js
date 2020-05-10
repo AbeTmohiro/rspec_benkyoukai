04b10880345713b06ede04fe0b13ec445ee1e39f
AbeTmohiro <tomohiro.abe@di-v.co.jp>
2020-05-10 12:40:16 +0900

    Delete "console log"in AutoReload.js

--- app/assets/javascripts/autoReload.js
+++ app/assets/javascripts/autoReload.js
@@ -28,7 +28,6 @@ $(function(){
       data: {id: last_message_id}
     })
     .done(function(messages) {
-      console.log(messages);
       if (messages.length !== 0) {
         let insertHTML = '';
         $.each(messages, function(i, message) {
